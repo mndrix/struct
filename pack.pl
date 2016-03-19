3b95b4cfddfcbae7ad73ec92730eea379f44fe61
Michael Hendricks <michael@ndrix.org>
2016-03-19 10:22:40 -0600

    Modernize pack layout

--- pack.pl
+++ pack.pl
@@ -3,10 +3,10 @@ title('Compound terms with named arguments').
 
 requires(lambda).
 
-version('0.0.1').
-download('http://packs.ndrix.com/struct/struct-0.0.1.tgz').
+version('0.1.0').
+download('https://github.com/mndrix/struct/archive/v0.1.0.zip').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
-home('http://packs.ndrix.com/struct/index.html').
+home('https://github.com/mndrix/struct').
